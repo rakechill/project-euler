2542df81475b38ea82c8bab099bc872d8f20b74f
Rachel Gregory <r_gregory1315@berkeley.edu>
2018-06-16 23:18:27 -0700

    prettier code

--- problem4.py
+++ problem4.py
@@ -2,25 +2,18 @@
 
 #1 figure out max and min's of your digits.
 def digit_ranges(digits):
-    """Takes in amount of digits and gives ranges for specified digits"""
-    range_min = pow(10, digits - 1)
-    range_max = pow(10, digits) - 1
+    range_min, range_max = pow(10, digits - 1), pow(10, digits) - 1
     mult_min, mult_max = pow(range_min, 2), pow(range_max, 2)
     return find_palindromes(range_min, range_max, mult_min, mult_max)
 
 #2 find your palindromes within that range.
 def find_palindromes(range_min, range_max, mult_min, mult_max):
-    palindromes = []
-    for num in range(mult_min, mult_max):
-        if str(num) == str(num)[::-1]:
-            palindromes.append(num)
+    palindromes = [num for num in range(mult_min, mult_max) if str(num) == str(num)[::-1]]
     return factors_of_palindromes(palindromes, range_min, range_max)
 
 #3 find the largest palindromes with factors in that range.
 def factors_of_palindromes(palindromes, range_min, range_max):
-    factors = []
-    for palindrome in palindromes:
-        for i in range(range_min, range_max + 1):
+    for palindrome in palindromes[::-1]:
+        for i in range(range_max, range_min, -1):
             if palindrome % i == 0 and palindrome / i >= range_min and palindrome / i <= range_max:
-                factors.append(palindrome)
-    return factors[len(factors) - 1]
+                return palindrome
